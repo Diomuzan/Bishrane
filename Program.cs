dbb41800d6d647e4b2a4201861941adbdbf70205
rincedal <rizotto247@gmail.com>
2023-10-13 11:08:53 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿string message = "Find what is (inside the parentheses)";
+int openingPosition = message.IndexOf('(');
+int closingPosition = message.IndexOf(')');
+openingPosition += 1;
+int length = closingPosition - openingPosition;
+Console.WriteLine(message.Substring(openingPosition, length) + "\n");
+
+string message1 = "What is the value <span>between the tags</span>?";
+int openingPosition1 = message1.IndexOf("<span>");
+int closingPosition1 = message1.IndexOf("</span>");
+openingPosition += 6;
+int length1 = closingPosition1 - openingPosition;
+Console.WriteLine(message.Substring(openingPosition, length1));
---
+++ README.md
@@ -0,0 +1,2 @@
+# Bishrane
+Bishrane is a console application developed in C# and lastly .NET.
