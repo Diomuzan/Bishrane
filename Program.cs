6f2bc1a45c684cadac720df422c5a05d38cbbc1d
rincedal <rizotto247@gmail.com>
2023-10-20 11:15:38 +0200

    Updated Bishrane

--- Program.cs
+++ Program.cs
@@ -40,6 +40,24 @@ while (true) {
     int closingPosition4 = message4.IndexOf(')');
     if (closingPosition4 == -1) break;
     int length4 = closingPosition4 - openingPosition4 - 1;
-    Console.WriteLine(message4.Substring(openingPosition4 + 1, length4));
+    Console.WriteLine(message4.Substring(openingPosition4 + 1, length4) + "\n");
     message4 = message4.Substring(closingPosition4 + 1);
+}
+
+string message5 = "(What if) I am (only interested) in the last (set of parentheses)?";
+int openingPosition5 = message5.LastIndexOf('(');
+openingPosition5 += 1;
+int closingPosition5 = message5.LastIndexOf(')');
+int length5 = closingPosition5 - openingPosition5;
+Console.WriteLine(message5.Substring(openingPosition5, length5) + "\n");
+
+string message6 = "(What if) there are (more than) one (set of parentheses)?";
+while (true) {
+    int openingPosition6 = message6.IndexOf('(');
+    if (openingPosition6 == -1) break;
+    openingPosition6 += 1;
+    int closingPosition6 = message6.IndexOf(')');
+    int length6 = closingPosition6 - openingPosition6; // Korrigierter Index
+    Console.WriteLine(message6.Substring(openingPosition6, length6));
+    message6 = message6.Substring(closingPosition6 + 1);
 }
